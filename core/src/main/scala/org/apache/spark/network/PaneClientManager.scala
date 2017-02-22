d0dfc55894fb95a5d6dd6d1e0b7b18111f2d712e
Christian <c.brueggemann94@gmail.com>
2017-02-22 16:52:18 +0100

    Extract something

--- core/src/main/scala/org/apache/spark/network/PaneClientManager.scala
+++ core/src/main/scala/org/apache/spark/network/PaneClientManager.scala
@@ -39,6 +39,14 @@ object PaneClientManager {
     paneClient
   }
 
+  def notifyFlow(srcHost: String, srcPort: Int,
+                 trgHost: String, trgPort: Int, logging: Logging): Unit = {
+    return
+
+    notifyFlow(InetAddress.getByName(srcHost), srcPort,
+      InetAddress.getByName(trgHost), trgPort, logging)
+  }
+
   def notifyFlow(srcHost: InetAddress, srcPort: Int,
                  trgHost: InetAddress, trgPort: Int, logging: Logging): Unit = {
     return
